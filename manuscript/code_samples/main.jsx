86041ebfdef6690619889b21ba62a5dd2fca077c
Swizec Teller <swizec@swizec.com>
2015-03-03 00:03:50 +0100

    base main component

---
+++ manuscript/code_samples/main.jsx
@@ -0,0 +1,31 @@
+
+//
+// Requires
+//
+
+
+// Example 1
+var React = require('react'),
+    _ = require('lodash'),
+    d3 = require('d3');
+
+
+
+//
+// H1BGraph
+//
+
+// Example 1
+var H1BGraph = React.createClass({
+    render: function () {
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    <svg width="700" height="500">
+
+                    </svg>
+                </div>
+            </div>
+        );
+    }
+});
