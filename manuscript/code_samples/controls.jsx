cd23c4a93dea411476a50f4f9a3eac8f98e18276
Swizec Teller <swizec@swizec.com>
2015-03-06 01:54:02 +0100

    finished creating controls bit

--- manuscript/code_samples/controls.jsx
+++ manuscript/code_samples/controls.jsx
@@ -252,3 +252,134 @@ var ControlRow = React.createClass({
 //
 // Example 8
 //
+var Controls = React.createClass({
+    // leanpub-start-insert
+    updateYearFilter: function (year, reset) {
+        var filter = function (d) {
+            return d.submit_date.getFullYear() == year;
+        };
+
+        if (reset || !year) {
+            filter = function () { return true; };
+        }
+
+        this.setState({yearFilter: filter});
+    },
+
+    getInitialState: function () {
+        return {yearFilter: function () { return true; }};
+    },
+    // leanpub-end-insert
+
+    render: function () {
+        var getYears = function (data) {
+            return _.keys(_.groupBy(data,
+                                    function (d) {
+                                        return d.submit_date.getFullYear()
+                                    }))
+                    .map(Number);
+        };
+
+        return (
+            <div>
+                <ControlRow data={this.props.data}
+                            getToggleValues={getYears}
+                            updateDataFilter={this.updateYearFilter} />
+            </div>
+        )
+    }
+});
+
+//
+// Example 9
+//
+var Controls = React.createClass({
+    updateYearFilter: function (year, reset) {
+        var filter = function (d) {
+            return d.submit_date.getFullYear() == year;
+        };
+
+        if (reset || !year) {
+            filter = function () { return true; };
+        }
+
+        this.setState({yearFilter: filter});
+    },
+
+    getInitialState: function () {
+        return {yearFilter: function () { return true; }};
+    },
+
+    render: function () {
+        var getYears = function (data) {
+            return _.keys(_.groupBy(data,
+                                    function (d) {
+                                        return d.submit_date.getFullYear()
+                                    }))
+                    .map(Number);
+        };
+
+        return (
+            <div>
+                <ControlRow data={this.props.data}
+                            getToggleValues={getYears}
+                            updateDataFilter={this.updateYearFilter} />
+            </div>
+        )
+    }
+});
+
+//
+// Example 10
+//
+var Controls = React.createClass({
+    updateYearFilter: function (year, reset) {
+        var filter = function (d) {
+            return d.submit_date.getFullYear() == year;
+        };
+
+        if (reset || !year) {
+            filter = function () { return true; };
+        }
+
+        this.setState({yearFilter: filter});
+    },
+
+    getInitialState: function () {
+        return {yearFilter: function () { return true; }};
+    },
+
+    // leanpub-start-insert
+    componentDidUpdate: function () {
+        this.props.updateDataFilter(
+            (function (filters) {
+                return function (d) {
+                    return filters.yearFilter(d)
+                };
+            })(this.state)
+        );
+    },
+
+    shouldComponentUpdate: function (nextProps, nextState) {
+        return !_.isEqual(this.state, nextState);
+    },
+    // leanpub-end-insert
+
+    render: function () {
+        var getYears = function (data) {
+            return _.keys(_.groupBy(data,
+                                    function (d) {
+                                        return d.submit_date.getFullYear()
+                                    }))
+                    .map(Number);
+        };
+
+        return (
+            <div>
+                <ControlRow data={this.props.data}
+                            getToggleValues={getYears}
+                            updateDataFilter={this.updateYearFilter} />
+            </div>
+        )
+    }
+});
--- manuscript/code_samples/main.jsx
+++ manuscript/code_samples/main.jsx
@@ -304,3 +304,254 @@ var H1BGraph = React.createClass({
         );
     }
 });
+
+//
+// Example 7
+//
+
+var H1BGraph = React.createClass({
+    componentWillMount: function () {
+        this.loadRawData();
+    },
+
+    getInitialState: function () {
+        return {rawData: []};
+    },
+
+    loadRawData: function () {
+        var dateFormat = d3.time.format("%m/%d/%Y");
+        d3.csv(this.props.url)
+          .row(function (d) {
+              if (!d['base salary']) {
+                  return null;
+              }
+
+              return {employer: d.employer,
+                      submit_date: dateFormat.parse(d['submit date']),
+                      start_date: dateFormat.parse(d['start date']),
+                      case_status: d['case status'],
+                      job_title: d['job title'],
+                      clean_job_title: this.cleanJobs(d['job title']),
+                      base_salary: Number(d['base salary']),
+                      salary_to: d['salary to'] ? Number(d['salary to']) : null,
+                      city: d.city,
+                      state: d.state};
+          }.bind(this))
+          .get(function (error, rows) {
+              if (error) {
+                  console.error(error);
+                  console.error(error.stack);
+              }else{
+                  this.setState({rawData: rows});
+              }
+          }.bind(this));
+    },
+
+    render: function () {
+        if (!this.state.rawData.length) {
+            return (
+                <h2>Loading data about 81,000 H1B visas in the software industry</h2>
+            );
+        }
+
+        // leanpub-start-insert
+        var params = {
+            bins: 20,
+            width: 500,
+            height: 500,
+            axisMargin: 83,
+            topMargin: 10,
+            bottomMargin: 5,
+            value: function (d) { return d.base_salary; }
+        },
+            fullWidth = 700;
+        // leanpub-end-insert
+
+        return (
+            // leanpub-start-insert
+            <div>
+            // leanpub-end-insert
+                <div className="row">
+                    <div className="col-md-12">
+                        <svg width={fullWidth} height={params.height}>
+                            <drawers.Histogram {...params} data={this.state.rawData} />
+                        </svg>
+                    </div>
+                </div>
+                // leanpub-start-insert
+                <Controls data={this.state.rawData} updateDataFilter={this.updateDataFilter} />
+            </div>
+            // leanpub-end-insert
+        );
+    }
+});
+
+
+//
+// Example 8
+//
+var H1BGraph = React.createClass({
+    loadRawData: function () {
+        var dateFormat = d3.time.format("%m/%d/%Y");
+        d3.csv(this.props.url)
+          .row(function (d) {
+              if (!d['base salary']) {
+                  return null;
+              }
+
+              return {employer: d.employer,
+                      submit_date: dateFormat.parse(d['submit date']),
+                      start_date: dateFormat.parse(d['start date']),
+                      case_status: d['case status'],
+                      job_title: d['job title'],
+                      clean_job_title: this.cleanJobs(d['job title']),
+                      base_salary: Number(d['base salary']),
+                      salary_to: d['salary to'] ? Number(d['salary to']) : null,
+                      city: d.city,
+                      state: d.state};
+          }.bind(this))
+          .get(function (error, rows) {
+              if (error) {
+                  console.error(error);
+                  console.error(error.stack);
+              }else{
+                  this.setState({rawData: rows});
+              }
+          }.bind(this));
+    },
+
+    componentWillMount: function () {
+        this.loadRawData();
+    },
+
+    getInitialState: function () {
+        return {rawData: [],
+                // leanpub-start-insert
+                dataFilter: function () { return true; }};
+        // leanpub-end-insert
+    },
+
+    // leanpub-start-insert
+    updateDataFilter: function (filter) {
+        this.setState({dataFilter: filter});
+    },
+    // leanpub-end-insert
+
+    render: function () {
+        if (!this.state.rawData.length) {
+            return (
+                <h2>Loading data about 81,000 H1B visas in the software industry</h2>
+            );
+        }
+
+        var params = {
+            bins: 20,
+            width: 500,
+            height: 500,
+            axisMargin: 83,
+            topMargin: 10,
+            bottomMargin: 5,
+            value: function (d) { return d.base_salary; }
+        },
+            fullWidth = 700;
+
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    <svg width={fullWidth} height={params.height}>
+                        <drawers.Histogram {...params} data={this.state.rawData} />
+                    </svg>
+                </div>
+            </div>
+        );
+    }
+
+});
+
+//
+// Example 9
+//
+var H1BGraph = React.createClass({
+    loadRawData: function () {
+        var dateFormat = d3.time.format("%m/%d/%Y");
+        d3.csv(this.props.url)
+          .row(function (d) {
+              if (!d['base salary']) {
+                  return null;
+              }
+
+              return {employer: d.employer,
+                      submit_date: dateFormat.parse(d['submit date']),
+                      start_date: dateFormat.parse(d['start date']),
+                      case_status: d['case status'],
+                      job_title: d['job title'],
+                      clean_job_title: this.cleanJobs(d['job title']),
+                      base_salary: Number(d['base salary']),
+                      salary_to: d['salary to'] ? Number(d['salary to']) : null,
+                      city: d.city,
+                      state: d.state};
+          }.bind(this))
+          .get(function (error, rows) {
+              if (error) {
+                  console.error(error);
+                  console.error(error.stack);
+              }else{
+                  this.setState({rawData: rows});
+              }
+          }.bind(this));
+    },
+
+    componentWillMount: function () {
+        this.loadRawData();
+    },
+
+    getInitialState: function () {
+        return {rawData: [],
+                dataFilter: function () { return true; }};
+    },
+
+    updateDataFilter: function (filter) {
+        this.setState({dataFilter: filter});
+    },
+
+    render: function () {
+        if (!this.state.rawData.length) {
+            return (
+                <h2>Loading data about 81,000 H1B visas in the software industry</h2>
+            );
+        }
+
+        var params = {
+            bins: 20,
+            width: 500,
+            height: 500,
+            axisMargin: 83,
+            topMargin: 10,
+            bottomMargin: 5,
+            value: function (d) { return d.base_salary; }
+        },
+            fullWidth = 700;
+
+
+        // leanpub-start-insert
+        var filteredData = this.state.rawData.filter(this.state.dataFilter);
+        // leanpub-end-insert
+
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <svg width={fullWidth} height={params.height}>
+                            // leanpub-start-delete
+                            <drawers.Histogram {...params} data={this.state.rawData} />
+                            // leanpub-end-delete
+                            <drawers.Histogram {...params} data={filteredData} />
+                        </svg>
+                    </div>
+                </div>
+                <Controls data={this.state.rawData} updateDataFilter={this.updateDataFilter} />
+            </div>
+        );
+    }
+
+});
