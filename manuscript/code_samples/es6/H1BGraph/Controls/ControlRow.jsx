b53a5d22f8dd91c9573ec5516b4af92b4231d88e
Swizec Teller <swizec@swizec.com>
2016-01-22 00:36:03 -0800

    Toggle row described-ish

--- manuscript/code_samples/es6/H1BGraph/Controls/ControlRow.jsx
+++ manuscript/code_samples/es6/H1BGraph/Controls/ControlRow.jsx
@@ -22,6 +22,108 @@ class ControlRow extends Component {
 export default ControlRow;
 
 
+//
+// Example 2
+//
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+import Toggle from './Toggle';
+
+class ControlRow extends Component {
+    // leanpub-start-insert
+    _addToggle(name) {
+        let key = `toggle-${name}`,
+            label = name;
+
+        if (this.props.capitalize) {
+            label = label.toUpperCase();
+        }
+
+        return (
+            <Toggle label={label}
+                    name={name}
+                    key={key}
+                    value={this.state.toggleValues[name]}
+                    onClick={::this.makePick} />
+        );
+    }
+    // leanpub-end-insert
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    // leanpub-start-insert
+                    {this.props
+                         .getToggleNames(this.props.data)
+                         .map((name) => this._addToggle(name))}
+                    // leanpub-end-insert
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ControlRow;
+
+
+//
+// Example 3
+//
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+import Toggle from './Toggle';
+
+class ControlRow extends Component {
+    // leanpub-start-insert
+    makePick(picked, newState) {
+
+    }
+
+    componentWillMount() {
+        let toggles = this.props.getToggleNames(this.props.data),
+            toggleValues = _.zipObject(toggles,
+                                       toggles.map(() => false));
+
+        this.state = {toggleValues: toggleValues};
+    }
+    // leanpub-end-insert
+
+    _addToggle(nme) {
+        let key = `toggle-${name}`,
+            label = name;
+
+        if (this.props.capitalize) {
+            label = label.toUpperCase();
+        }
+
+        return (
+            <Toggle label={label}
+                    name={name}
+                    key={key}
+                    value={this.state.toggleValues[name]}
+                    onClick={::this.makePick} />
+        );
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    {this.props
+                         .getToggleNames(this.props.data)
+                         .map((name) => this._addToggle(name))}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ControlRow;
+
+
 /// -----
 
 
