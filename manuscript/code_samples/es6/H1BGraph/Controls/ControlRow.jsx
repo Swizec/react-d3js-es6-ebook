e8a573430132ff00add2df266a4cac7909f54163
Swizec Teller <swizec@swizec.com>
2016-01-24 18:55:27 -0800

    Toggles propagating up to ControlRow

--- manuscript/code_samples/es6/H1BGraph/Controls/ControlRow.jsx
+++ manuscript/code_samples/es6/H1BGraph/Controls/ControlRow.jsx
@@ -124,6 +124,69 @@ class ControlRow extends Component {
 export default ControlRow;
 
 
+//
+// Example 4
+//
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+import Toggle from './Toggle';
+
+class ControlRow extends Component {
+    makePick(picked, newState) {
+        // leanpub-start-insert
+        let toggleValues = this.state.toggleValues;
+
+        toggleValues = _.mapValues(toggleValues,
+                                   (value, key) => newState && key == picked);
+
+        // if newState is false, we want to reset
+        this.props.updateDataFilter(picked, !newState);
+
+        this.setState({toggleValues: toggleValues});
+        // leanpub-end-insert
+    }
+
+    componentWillMount() {
+        let toggles = this.props.getToggleNames(this.props.data),
+            toggleValues = _.zipObject(toggles,
+                                       toggles.map(() => false));
+
+        this.state = {toggleValues: toggleValues};
+    }
+    _addToggle(nme) {
+        let key = `toggle-${name}`,
+            label = name;
+
+        if (this.props.capitalize) {
+            label = label.toUpperCase();
+        }
+
+        return (
+            <Toggle label={label}
+                    name={name}
+                    key={key}
+                    value={this.state.toggleValues[name]}
+                    onClick={::this.makePick} />
+        );
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    {this.props
+                         .getToggleNames(this.props.data)
+                         .map((name) => this._addToggle(name))}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ControlRow;
+
+
 /// -----
 
 
