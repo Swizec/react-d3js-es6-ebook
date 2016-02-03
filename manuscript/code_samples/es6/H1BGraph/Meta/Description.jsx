dd9cf814378b59e8173a4f05ab6cfc2f600b9aa2
Swizec Teller <swizec@swizec.com>
2016-02-03 01:32:39 -0800

    Description writeup done

--- manuscript/code_samples/es6/H1BGraph/Meta/Description.jsx
+++ manuscript/code_samples/es6/H1BGraph/Meta/Description.jsx
@@ -19,6 +19,312 @@ class Description extends Meta {
 
 export default Description;
 
+//
+// Example 2
+//
+import React, { Component } from 'react';
+import d3 from 'd3';
+
+import Meta from './BaseComponent';
+import StatesMap from './StatesMap';
+
+
+class Description extends Meta {
+    // leanpub-start-insert
+    getAllDataByYear(year, data) {
+        data || (data = this.props.allData);
+
+        return data.filter((d) => d.submit_date.getFullYear() == year);
+    }
+
+    getAllDataByUSState(USstate, data) {
+        data || (data = this.props.allData);
+
+        return data.filter((d) => d.state == USstate);
+    }
+    // leanpub-end-insert
+
+    render() {
+        return (
+            <p className="lead">This is a description</p>
+        )
+    }
+}
+
+export default Description;
+
+
+//
+// Example 3
+//
+import React, { Component } from 'react';
+import d3 from 'd3';
+
+import Meta from './BaseComponent';
+import StatesMap from './StatesMap';
+
+
+class Description extends Meta {
+    getAllDataByYear(year, data) {
+        data || (data = this.props.allData);
+
+        return data.filter((d) => d.submit_date.getFullYear() == year);
+    }
+
+    getAllDataByUSState(USstate, data) {
+        data || (data = this.props.allData);
+
+        return data.filter((d) => d.state == USstate);
+    }
+
+    // leanpub-start-insert
+    getPreviousYearFragment() {
+        let years = this.getYears().map(Number),
+            fragment;
+
+        if (years.length > 1) {
+            fragment = "";
+        }else if (years[0] == 2012) {
+            fragment = "";
+        }else{
+            let year = years[0],
+                lastYear = this.getAllDataByYear(year-1),
+                USstates = this.getUSStates();
+
+
+            if (USstates.length == 1) {
+                lastYear = this.getAllDataByState(USstates[0], lastYear);
+            }
+
+            if (this.props.data.length/lastYear.length > 2) {
+                let times_more = (this.props.data.length/lastYear.length).toFixed();
+
+                fragment = `, ${times_more} times more than the year before`;
+            }else{
+                let percent = ((1-lastYear.length/this.props.data.length)*100).toFixed();
+
+                fragment = `, ${Math.abs(percent)}% ${percent > 0 ? "more" : "less"} than the year before`;
+            }
+        }
+
+        return fragment;
+    }
+    // leanpub-end-insert
+
+    render() {
+        return (
+            <p className="lead">This is a description</p>
+        )
+    }
+}
+
+export default Description;
+
+
+//
+// Example 4
+//
+import React, { Component } from 'react';
+import d3 from 'd3';
+
+import Meta from './BaseComponent';
+import StatesMap from './StatesMap';
+
+
+class Description extends Meta {
+    getAllDataByYear(year, data) {
+        data || (data = this.props.allData);
+
+        return data.filter((d) => d.submit_date.getFullYear() == year);
+    }
+
+    getAllDataByUSState(USstate, data) {
+        data || (data = this.props.allData);
+
+        return data.filter((d) => d.state == USstate);
+    }
+
+    getPreviousYearFragment() {
+        let years = this.getYears().map(Number),
+            fragment;
+
+        if (years.length > 1) {
+            fragment = "";
+        }else if (years[0] == 2012) {
+            fragment = "";
+        }else{
+            let year = years[0],
+                lastYear = this.getAllDataByYear(year-1),
+                USstates = this.getUSStates();
+
+
+            if (USstates.length == 1) {
+                lastYear = this.getAllDataByState(USstates[0], lastYear);
+            }
+
+            if (this.props.data.length/lastYear.length > 2) {
+                let times_more = (this.props.data.length/lastYear.length).toFixed();
+
+                fragment = `, ${times_more} times more than the year before`;
+            }else{
+                let percent = ((1-lastYear.length/this.props.data.length)*100).toFixed();
+
+                fragment = `, ${Math.abs(percent)}% ${percent > 0 ? "more" : "less"} than the year before`;
+            }
+        }
+
+        return fragment;
+    }
+
+    // leanpub-start-insert
+    getYearFragment() {
+        let years = this.getYears(),
+            fragment;
+
+        if (years.length > 1) {
+            fragment = "";
+        }else{
+            fragment = "In "+years[0];
+        }
+
+        return fragment;
+    }
+
+    getUSStateFragment() {
+        let states = this.getUSStates(),
+            fragment;
+
+        if (states.length > 1) {
+            fragment = "US";
+        }else{
+            fragment = StatesMap[states[0].toUpperCase()];
+        }
+
+        return fragment;
+    }
+    // leanpub-end-insert
+
+    render() {
+        return (
+            <p className="lead">This is a description</p>
+        )
+    }
+}
+
+export default Description;
+
+
+//
+// Example 5
+//
+import React, { Component } from 'react';
+import d3 from 'd3';
+
+import Meta from './BaseComponent';
+import StatesMap from './StatesMap';
+
+
+class Description extends Meta {
+    getAllDataByYear(year, data) {
+        data || (data = this.props.allData);
+
+        return data.filter((d) => d.submit_date.getFullYear() == year);
+    }
+
+    getAllDataByUSState(USstate, data) {
+        data || (data = this.props.allData);
+
+        return data.filter((d) => d.state == USstate);
+    }
+
+    getPreviousYearFragment() {
+        let years = this.getYears().map(Number),
+            fragment;
+
+        if (years.length > 1) {
+            fragment = "";
+        }else if (years[0] == 2012) {
+            fragment = "";
+        }else{
+            let year = years[0],
+                lastYear = this.getAllDataByYear(year-1),
+                USstates = this.getUSStates();
+
+
+            if (USstates.length == 1) {
+                lastYear = this.getAllDataByState(USstates[0], lastYear);
+            }
+
+            if (this.props.data.length/lastYear.length > 2) {
+                let times_more = (this.props.data.length/lastYear.length).toFixed();
+
+                fragment = `, ${times_more} times more than the year before`;
+            }else{
+                let percent = ((1-lastYear.length/this.props.data.length)*100).toFixed();
+
+                fragment = `, ${Math.abs(percent)}% ${percent > 0 ? "more" : "less"} than the year before`;
+            }
+        }
+
+        return fragment;
+    }
+
+    getYearFragment() {
+        let years = this.getYears(),
+            fragment;
+
+        if (years.length > 1) {
+            fragment = "";
+        }else{
+            fragment = "In "+years[0];
+        }
+
+        return fragment;
+    }
+
+    getUSStateFragment() {
+        let states = this.getUSStates(),
+            fragment;
+
+        if (states.length > 1) {
+            fragment = "US";
+        }else{
+            fragment = StatesMap[states[0].toUpperCase()];
+        }
+
+        return fragment;
+    }
+
+    render() {
+        // leanpub-start-insert
+        let formatter = this.getFormatter(),
+            mean = d3.mean(this.props.data,
+                           (d) => d.base_salary),
+            deviation = d3.deviation(this.props.data,
+                                     (d) => d.base_salary);
+
+        let yearFragment = this.getYearFragment(),
+            USStateFragment = this.getUSStateFragment(),
+            previousYearFragment = this.getPreviousYearFragment(),
+            N = formatter(this.props.data.length),
+            min_salary = formatter(mean-deviation),
+            max_salary = formatter(mean+deviation);
+        // leanpub-end-insert
+
+        return (
+            // leanpub-start-delete
+            <p className="lead">This is a description</p>
+            // leanpub-end-delete
+            // leanpub-start-insert
+            <p className="lead">{yearFragment.length ? yearFragment : "Since 2012"} the {USStateFragment} software industry {yearFragment.length ? "gave" : "has given"} jobs to {N} foreign nationals{previousYearFragment}. Most of them made between ${min_salary} and ${max_salary} per year.</p>
+            // leanpub-end-insert
+        );
+    }
+
+}
+
+export default Description;
+
 
 // ---
 import React, { Component } from 'react';
